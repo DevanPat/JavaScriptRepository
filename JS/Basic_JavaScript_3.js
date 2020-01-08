33920b3b2b434749dab772c6c75d6f643e026dbd
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-01-07 21:23:03 -0600

    changes

--- JS/Basic_JavaScript_3.js
+++ JS/Basic_JavaScript_3.js
@@ -2,3 +2,7 @@ function my_Third_Function(a, b) {
 document.getElementById("Math").innerHTML = a + b;
 }
 
+function my_Fourth_Function()   {
+    var Subtraction = 6 - 3;
+    document.getElementById("Subtraction").innerHTML = "6 - 3 = " + Subtraction;
+}
