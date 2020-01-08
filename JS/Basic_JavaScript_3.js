7025fb53f2962385902d4d9d8c5471de4e6c00bc
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-01-07 21:43:44 -0600

    changes

--- JS/Basic_JavaScript_3.js
+++ JS/Basic_JavaScript_3.js
@@ -6,3 +6,19 @@ function my_Fourth_Function()   {
     var Subtraction = 6 - 3;
     document.getElementById("Subtraction").innerHTML = "6 - 3 = " + Subtraction;
 }
+
+function my_Fifth_Function()    {
+    var Multiplication = 10 * 7;
+    document.getElementById("Multiplication").innerHTML = "10 * 7 = " + Multiplication;
+}
+
+function my_Sixth_Function()    {
+    var Division = 15 * 35;
+    document.getElementById("Division").innerHTML = "15 * 35 = " + Division;
+}
+
+function my_Seventh_Function()  {
+    var Calculatedis = (15-10) * 5 / 2 + 9;
+    document.getElementById("Calculatedis").innerHTML = "15 minus 10, multiplied by 5, divided in 2, plus 9 equals " + Calculatedis;
+}
+
