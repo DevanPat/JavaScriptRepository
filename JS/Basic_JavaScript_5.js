3511afa244fd4081b1944a314c85ab31db9c182e
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-01-10 19:54:25 -0600

    html js 5

--- JS/Basic_JavaScript_5.js
+++ JS/Basic_JavaScript_5.js
@@ -1,3 +1,54 @@
 document.write(typeof "Apple");
 
 document.write(typeof 7);
+
+function my_Function()  {
+    document.getElementById("Nan").innerHTML = 0/0;
+}
+
+function my_Function2()  {
+    document.getElementById("Purple").innerHTML = isNaN('Purple');
+}
+
+
+function my_Function3()  {
+    document.getElementById("Number").innerHTML = isNaN('1404034');
+}
+
+document.write(-3E410);
+
+document.write(2E410);
+
+document.write(1 > 2);
+
+console.log(3 + 3);
+
+document.write("9" + 8);
+
+console.log(10 < 3);
+
+document.write(12 == 12);
+
+document.write(30 == 40);
+
+X = 7;
+Y = 7;
+document.write(X === Y);
+
+A = 20;
+B = "90";
+document.write(A === B);
+
+N = 30;
+M = "30";
+document.write(N === M);
+
+P = 11/21/1972;
+Q = 11/21/1974;
+document.write(P === Q);
+
+
+
+
+
+
