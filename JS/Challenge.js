fdf1e51939bbdba936eba8c0ae3558adce69d9d0
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-01-13 21:19:20 -0600

    committing

---
+++ JS/Challenge.js
@@ -0,0 +1,9 @@
+function Dog(name, breed, age, color)   {
+    this.Name = name;
+    this.Breed = breed;
+    this.Age = age;
+    this.Color = color;
+}
+
+var myDog = new Dog("Joey", "Australian Shepherd", 2, "Blue Merle");
+var sallysDog = new Dog("Meow", "German Shepherd", 3, "Black & Tan")
