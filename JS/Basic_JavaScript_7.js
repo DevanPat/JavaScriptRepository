299acc34614b930df08e640bcd21ca28ce44d88a
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-01-14 15:59:01 -0600

    committing

--- JS/Basic_JavaScript_7.js
+++ JS/Basic_JavaScript_7.js
@@ -1,6 +1,35 @@
-function Vote_Function()    {
-    var Age, Can_vote;
-    Age = document.getElementById("Age").value;
-    Can_vote = (Age < 18) ? "You are too young":"You are old enough";
-    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+var b = 100;
+function Divide_numbers()   {
+    document.write(300 / b + "<br>");
+}
+function Multiply_numbers() {
+    var a = 25;
+    document.write(a * 11);
+}
+
+
+function Add_numbers() {
+    console.log(b + 32);
+}
+
+function get_Date() {
+    if (new Date().getHours() < 20) {
+        document.getElementById("Greet").innerHTML = "How are you?";
+    }
+}
+
+function bungee_Function()  {
+    var Bungee;
+    Age = document.getElementById("Age").value
+    if  (Age >= 21) {
+        Bungee = "You are able to bungee jump!";
+    }
+    else    {
+        Bungee = "You are not old enough to jump!";
+    }
+    document.getElementById("Jump").innerHTML = Bungee;
+}
+
+if (30 > 3) {
+    document.write("The left number is bigger then the right number.")
 }
