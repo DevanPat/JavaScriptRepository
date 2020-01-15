e3373a7f2a4b1250b3f64a61470ddb351053134c
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-01-14 22:38:51 -0600

    changes

--- JS/Basic_JavaScript_8.js
+++ JS/Basic_JavaScript_8.js
@@ -21,3 +21,47 @@ function co_tate()  {
     var sentence = sec_1.concat(sec_2, sec_3, sec_4);
     document.getElementById("Concatenate").innerHTML = sentence;
 }
+
+function slice_Method() {
+    var Sentence = "Jim made gumbo for 50 people."
+    var Section = Sentence.slice(4, 9);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function my_Function()  {
+    var text = document.getElementById("toUpper").innerHTML;
+    document.getElementById("toUpper").innerHTML = text.toUpperCase();
+}
+
+    function two_Function()  {
+var str = "Where are my shoelaces? Did the dog chew them up?";
+var pos = str.search("shoelaces");
+document.getElementById("Frogs").innerHTML = pos;
+}
+
+function string_Method()   {
+var X =500;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method() {
+    var X = 56789.877066098989321
+    document.getElementById("Precision").innerHTML = X.toPrecision(8);
+}
+
+function to_Fixed() {
+    var x= 20.9749
+    document.getElementById("toFixed").innerHTML =
+    x.toFixed(12) + "<br>" +
+    x.toFixed(3) + "<br>" +
+    x.toFixed(8) + "<br>" +
+    x.toFixed(11);
+}
+
+function value_Of() {
+    var a = 120;
+    document.getElementById("valueOf").innerHTML =
+    a.valueOf() + "<br>" +
+    (120).valueOf() + "<br>" +
+    (117 + 3).valueOf();
+}
